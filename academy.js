418a633b72c8f307b01ffb04126f178c6b856202
Raul henrique <121063862+raulcacula@users.noreply.github.com>
2024-01-04 21:01:39 -0300

    Academia-lading-page
    Site lading page para academia ,facil de usar e responsivo.
    Feito com HTML, CSS  e  JS

---
+++ academy.js
@@ -0,0 +1,15 @@
+
+var barsLateral = document.querySelector('.bar-lateral')
+var closeBtn = document.querySelector('.close')
+
+function closeBar(){
+    if(closeBtn = barsLateral.style.display = 'none'){
+
+    }
+}
+
+function openBars(){
+    if(barsLateral.style.display = 'flex'){
+
+    }
+}
